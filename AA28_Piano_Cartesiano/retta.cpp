e4b50d8f682092c82f1ed7d89c34cfad05861527
leonardosilvagni <leonardo.silvagni@gmail.com>
2018-08-24 12:01:26 +0200

    Added the header

---
+++ AA28_Piano_Cartesiano/retta.cpp
@@ -0,0 +1,9 @@
+//
+//  retta.cpp
+//  AA28_Piano_Cartesiano
+//
+//  Created by Leonardo Silvagni on 24/08/18.
+//  Copyright © 2018 Leonardo Silvagni. All rights reserved.
+//
+
+#include "retta.hpp"
---
+++ AA28_Piano_Cartesiano/retta.hpp
@@ -0,0 +1,14 @@
+//
+//  retta.hpp
+//  AA28_Piano_Cartesiano
+//
+//  Created by Leonardo Silvagni on 24/08/18.
+//  Copyright © 2018 Leonardo Silvagni. All rights reserved.
+//
+
+#ifndef retta_hpp
+#define retta_hpp
+
+#include <stdio.h>
+
+#endif /* retta_hpp */
