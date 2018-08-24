09b5871fe555138a5cda6f2b00a14a15f14d068b
leonardosilvagni <leonardo.silvagni@gmail.com>
2018-08-24 10:51:42 +0200

    Initial Commit

---
+++ AA28_Piano_Cartesiano/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AA28_Piano_Cartesiano
+//
+//  Created by Leonardo Silvagni on 24/08/18.
+//  Copyright © 2018 Leonardo Silvagni. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
