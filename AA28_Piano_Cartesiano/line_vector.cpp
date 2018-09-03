69b1bb0e79b5d857621fc2f39f8ee38ef29f8df4
leonardosilvagni <leonardo.silvagni@gmail.com>
2018-09-03 12:16:44 +0200

    to finish

---
+++ AA28_Piano_Cartesiano/line_vector.cpp
@@ -0,0 +1,9 @@
+//
+//  line_vector.cpp
+//  AA28_Piano_Cartesiano
+//
+//  Created by Leonardo Silvagni on 03/09/18.
+//  Copyright © 2018 Leonardo Silvagni. All rights reserved.
+//
+
+#include "line_vector.hpp"
---
+++ AA28_Piano_Cartesiano/line_vector.hpp
@@ -0,0 +1,14 @@
+//
+//  line_vector.hpp
+//  AA28_Piano_Cartesiano
+//
+//  Created by Leonardo Silvagni on 03/09/18.
+//  Copyright © 2018 Leonardo Silvagni. All rights reserved.
+//
+
+#ifndef line_vec_hpp
+#define line_vec_hpp
+#include <iostream>
+#include "retta.hpp"
+
+#endif /* line_vector_hpp */
